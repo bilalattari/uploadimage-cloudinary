1a4baed6494cfe2b74e098346aa55cb5a8f358ec
Bilal raza <attari1235@gmail.com>
2024-11-19 21:05:00 +0500

    api added

--- src/app/page.js
+++ src/app/page.js
@@ -4,8 +4,8 @@ import SearchBar from "@/components/SearchBar";
 import Link from "next/link";
 
 export default async function Home() {
-  // const { images } = await getImages();
-  // console.log("images=>", images);
+  const { images } = await getImages();
+  console.log("images=>", images);
   return (
     <div className="container mx-auto py-10">
       <div className="flex">
@@ -14,16 +14,19 @@ export default async function Home() {
           <button className="bg-blue-300 rounded py-2 px-4">Add Image</button>
         </Link>
       </div>
-      {/* <div className="flex flex-wrap gap-3 my-10">
+      <div className="flex flex-wrap gap-3 my-10">
         {images?.map((data) => {
           return (
-            <div className="bg-gray-100 rounded p-2 flex flex-col">
+            <div
+              key={data._id}
+              className="bg-gray-100 rounded p-2 flex flex-col"
+            >
               <img className="h-40 w-full rounded" src={data.image} />
               <h1 className="font-bold mt-2">{data.title}</h1>
             </div>
           );
         })}
-      </div> */}
+      </div>
     </div>
   );
 }
